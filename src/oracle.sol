61322b4b6b67d9c67aa1a2c994272ba758bc4d20
rashadalh <alhaddad.rashad@gmail.com>
2024-05-29 17:50:53 -0500

    adds simple oracle for the temperature

---
+++ src/oracle.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.8.0;
+
+// simple contract to act as a temperature oracle
+
+interface Ioracle {
+    function setTemperature(uint256 _temperature) external;
+    function getTemperature() external view returns (uint256);
+}
+
+contract oracle is Ioracle {
+    address public owner;
+    uint256 public temperature;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function setTemperature(uint256 _temperature) public onlyOwner() {
+        temperature = _temperature;
+    }
+
+    function getTemperature() public view returns (uint256) {
+        return temperature;
+    }
+}
